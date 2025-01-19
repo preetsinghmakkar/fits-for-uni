84cd96ad886b4e1eeb0ee51dcc0f3faa2ca17f45
preetsinghmakkar <preetsinghmakkar154@gmail.com>
2025-01-19 17:05:48 +0530

    changed .env variables

--- server-fits-for-uni/utils/generatedAccessToken.js
+++ server-fits-for-uni/utils/generatedAccessToken.js
@@ -1,13 +1,8 @@
 import jwt from 'jsonwebtoken'
 
 const generatedAccessToken = async(userId)=>{
-    // const token = await jwt.sign({ id : userId},
-    //     process.env.SECRET_KEY_ACCESS_TOKEN,
-    //     { expiresIn : '5h'}
-    // )
-
     const token = await jwt.sign({ id : userId},
-        c559e9117bea57fa7f2379c909759248ca7c419945ef519b614a5314d8b8c246,
+        process.env.SECRET_KEY_ACCESS_TOKEN,
         { expiresIn : '5h'}
     )
 
