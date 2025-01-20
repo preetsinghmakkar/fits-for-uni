ce30da875f10151dc9b24d03b91607f0dce5725b
preetsinghmakkar <preetsinghmakkar154@gmail.com>
2025-01-20 12:28:58 +0530

    removed cloudinary data

--- server-fits-for-uni/utils/uploadImageClodinary.js
+++ server-fits-for-uni/utils/uploadImageClodinary.js
@@ -1,17 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary';
 
-// cloudinary.config({
-//     cloud_name : process.env.CLODINARY_CLOUD_NAME,
-//     api_key : process.env.CLODINARY_API_KEY,
-//     api_secret : process.env.CLODINARY_API_SECRET_KEY
-// })
-
 cloudinary.config({
-    cloud_name : "dfel75ojr",
-    api_key : "631299454141755",
-    api_secret : "e_BbD_-JecrtIr5iLY6Edfp_rXM"
+    cloud_name : process.env.CLODINARY_CLOUD_NAME,
+    api_key : process.env.CLODINARY_API_KEY,
+    api_secret : process.env.CLODINARY_API_SECRET_KEY
 })
-
 
 const uploadImageClodinary = async(image)=>{
     const buffer = image?.buffer || Buffer.from(await image.arrayBuffer())
