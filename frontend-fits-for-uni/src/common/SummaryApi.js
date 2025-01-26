c5cd6d290b627b328ba1a14e18650ac9994b65c5
Harshit Bansal <84428515+harshitbansal184507@users.noreply.github.com>
2025-01-26 07:41:37 +0530

    update

--- frontend-fits-for-uni/src/common/SummaryApi.js
+++ frontend-fits-for-uni/src/common/SummaryApi.js
@@ -1,158 +1,158 @@
-export const baseURL = import.meta.env.VITE_API_URL;
+export const baseURL = import.meta.env.VITE_API_URL
 
 const SummaryApi = {
     register : {
-        url : `${baseURL}/api/user/register`,
+        url : '/api/user/register',
         method : 'post'
     },
     login : {
-        url : `${baseURL}/api/user/login`,
+        url : '/api/user/login',
         method : 'post'
     },
     forgot_password : {
-        url : `${baseURL}/api/user/forgot-password`,
+        url : "/api/user/forgot-password",
         method : 'put'
     },
     forgot_password_otp_verification : {
-        url : `${baseURL}/api/user/verify-forgot-password-otp`,
+        url : 'api/user/verify-forgot-password-otp',
         method : 'put'
     },
     resetPassword : {
-        url : `${baseURL}/api/user/reset-password`,
+        url : "/api/user/reset-password",
         method : 'put'
     },
     refreshToken : {
-        url : `${baseURL}/api/user/refresh-token`,
+        url : 'api/user/refresh-token',
         method : 'post'
     },
     userDetails : {
-        url : `${baseURL}/api/user/user-details`,
+        url : '/api/user/user-details',
         method : "get"
     },
     logout : {
-        url : `${baseURL}/api/user/logout`,
+        url : "/api/user/logout",
         method : 'get'
     },
     uploadAvatar : {
-        url : `${baseURL}/api/user/upload-avatar`,
+        url : "/api/user/upload-avatar",
         method : 'put'
     },
     updateUserDetails : {
-        url : `${baseURL}/api/user/update-user`,
+        url : '/api/user/update-user',
         method : 'put'
     },
     addCategory : {
-        url : `${baseURL}/api/category/add-category`,
+        url : '/api/category/add-category',
         method : 'post'
     },
     uploadImage : {
-        url : `${baseURL}/api/file/upload`,
+        url : '/api/file/upload',
         method : 'post'
     },
     getCategory : {
-        url : `${baseURL}/api/category/get`,
+        url : '/api/category/get',
         method : 'get'
     },
     updateCategory : {
-        url : `${baseURL}/api/category/update`,
+        url : '/api/category/update',
         method : 'put'
     },
     deleteCategory : {
-        url : `${baseURL}/api/category/delete`,
+        url : '/api/category/delete',
         method : 'delete'
     },
     createSubCategory : {
-        url : `${baseURL}/api/subcategory/create`,
+        url : '/api/subcategory/create',
         method : 'post'
     },
     getSubCategory : {
-        url : `${baseURL}/api/subcategory/get`,
+        url : '/api/subcategory/get',
         method : 'post'
     },
     updateSubCategory : {
-        url : `${baseURL}/api/subcategory/update`,
+        url : '/api/subcategory/update',
         method : 'put'
     },
     deleteSubCategory : {
-        url : `${baseURL}/api/subcategory/delete`,
+        url : '/api/subcategory/delete',
         method : 'delete'
     },
     createProduct : {
-        url : `${baseURL}/api/product/create`,
+        url : '/api/product/create',
         method : 'post'
     },
     getProduct : {
-        url : `${baseURL}/api/product/get`,
+        url : '/api/product/get',
         method : 'post'
     },
     getProductByCategory : {
-        url : `${baseURL}/api/product/get-product-by-category`,
+        url : '/api/product/get-product-by-category',
         method : 'post'
     },
     getProductByCategoryAndSubCategory : {
-        url : `${baseURL}/api/product/get-pruduct-by-category-and-subcategory`,
+        url : '/api/product/get-pruduct-by-category-and-subcategory',
         method : 'post'
     },
     getProductDetails : {
-        url : `${baseURL}/api/product/get-product-details`,
+        url : '/api/product/get-product-details',
         method : 'post'
     },
     updateProductDetails : {
-        url : `${baseURL}/api/product/update-product-details`,
+        url : "/api/product/update-product-details",
         method : 'put'
     },
     deleteProduct : {
-        url : `${baseURL}/api/product/delete-product`,
+        url : "/api/product/delete-product",
         method : 'delete'
     },
     searchProduct : {
-        url : `${baseURL}/api/product/search-product`,
+        url : '/api/product/search-product',
         method : 'post'
     },
     addTocart : {
-        url : `${baseURL}/api/cart/create`,
+        url : "/api/cart/create",
         method : 'post'
     },
     getCartItem : {
-        url : `${baseURL}/api/cart/get`,
+        url : '/api/cart/get',
         method : 'get'
     },
     updateCartItemQty : {
-        url : `${baseURL}/api/cart/update-qty`,
+        url : '/api/cart/update-qty',
         method : 'put'
     },
     deleteCartItem : {
-        url : `${baseURL}/api/cart/delete-cart-item`,
+        url : '/api/cart/delete-cart-item',
         method : 'delete'
     },
     createAddress : {
-        url : `${baseURL}/api/address/create`,
+        url : '/api/address/create',
         method : 'post'
     },
     getAddress : {
-        url : `${baseURL}/api/address/get`,
+        url : '/api/address/get',
         method : 'get'
     },
     updateAddress : {
-        url : `${baseURL}/api/address/update`,
+        url : '/api/address/update',
         method : 'put'
     },
     disableAddress : {
-        url : `${baseURL}/api/address/disable`,
+        url : '/api/address/disable',
         method : 'delete'
     },
     CashOnDeliveryOrder : {
-        url : `${baseURL}/api/order/cash-on-delivery`,
+        url : "/api/order/cash-on-delivery",
         method : 'post'
     },
     payment_url : {
-        url : `${baseURL}/api/order/checkout`,
+        url : "/api/order/checkout",
         method : 'post'
     },
     getOrderItems : {
-        url : `${baseURL}/api/order/order-list`,
+        url : '/api/order/order-list',
         method : 'get'
     }
 }
 
-export default SummaryApi;
+export default SummaryApi
